1cbab846f250ac1b4d118463f89d39114d7eec69
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-13 00:55:15 +0330

    Add WooCommerce support

--- simple theme/functions.php
+++ simple theme/functions.php
@@ -24,6 +24,12 @@ function my_theme_add_elementor_support() {
 }
 add_action('after_setup_theme', 'my_theme_add_elementor_support');
 
+// Add WooCommerce support
+function my_theme_add_woocommerce_support() {
+    add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'my_theme_add_woocommerce_support');
+
 // Load theme text domain for translation (Need to be completed!)
 function load_theme_textdomain() {
     load_theme_textdomain('your-text-domain', get_template_directory() . '/languages');
