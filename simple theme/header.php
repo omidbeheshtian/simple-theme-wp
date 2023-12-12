0047633a031c411a6dd02a61b549d32820bb9b08
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-13 00:38:30 +0330

    Update header.php

--- simple theme/header.php
+++ simple theme/header.php
@@ -2,9 +2,10 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <title><?php bloginfo( 'name' ); ?></title>
-    <?php wp_head() ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -13,9 +14,8 @@
 
     <header class="site-header">
 
-        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 
-
-        <h4><?php bloginfo( 'description' ); ?></h4>
+        <h4><?php bloginfo('description'); ?></h4>
 
     </header>
